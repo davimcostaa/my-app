90860a2eb0780be06e48f72a8c70793bf36363ec
Davi Costa <davim.costa@outlook.com>
2023-08-16 21:50:45 -0300

    Primeiro app.

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { Image } from 'expo-image';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+      source={require('./assets/background.jpg')} style={styles.imageBackground}
+      >
+            <Text style={styles.texto}>Seja bem-vindo (a)</Text>
+      <Image
+            style={styles.image}
+            source={require('./assets/lua.png')}
+          />
+          <StatusBar style="auto" />
+
+          <Button
+          title="Clique aqui"
+          onPress={() => Alert.alert('Se a lua toca no mar...')}
+          />
+      </ImageBackground>
+
     </View>
   );
 }
@@ -13,8 +28,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#75ae97',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  
+  image: {
+    width: 300,
+    height: 300,
+  },
+
+  texto: {
+    fontSize: 20,
+    color: 'black',
+    marginBottom: 50,
+    backgroundColor: 'white',
+    fontFamily: 'Roboto'
+  },
+
+  imageBackground: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+    width: '100%'
   },
 });
